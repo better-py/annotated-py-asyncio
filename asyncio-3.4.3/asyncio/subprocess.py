35c6c9caca721e6fb818f71f0443dc9b1c086995
hhstore <hhstore@qq.com>
2017-03-11 17:39:20 +0800

    update - 子进程部分, 通过子进程执行 shell 命令.

--- asyncio-3.4.3/asyncio/subprocess.py
+++ asyncio-3.4.3/asyncio/subprocess.py
@@ -93,6 +93,9 @@ class SubprocessStreamProtocol(streams.FlowControlMixin,
         self._transport = None
 
 
+#
+# 进程:
+#
 class Process:
     def __init__(self, transport, protocol, loop):
         self._transport = transport
@@ -186,6 +189,9 @@ class Process:
         return (stdout, stderr)
 
 
+#
+# 创建子进程 shell:
+#
 @coroutine
 def create_subprocess_shell(cmd, stdin=None, stdout=None, stderr=None,
                             loop=None, limit=streams._DEFAULT_LIMIT, **kwds):
@@ -193,6 +199,10 @@ def create_subprocess_shell(cmd, stdin=None, stdout=None, stderr=None,
         loop = events.get_event_loop()
     protocol_factory = lambda: SubprocessStreamProtocol(limit=limit,
                                                         loop=loop)
+
+    #
+    # 执行 shell 命令:
+    #
     transport, protocol = yield from loop.subprocess_shell(
                                             protocol_factory,
                                             cmd, stdin=stdin, stdout=stdout,
